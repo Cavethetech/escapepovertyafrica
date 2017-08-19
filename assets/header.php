ed333c04a0b12bfebee8ae28c7628bea51c861d2
Cavendish Mwangi <cavendish.mwangi@meltwater.org>
2017-08-19 09:10:56 +0000

    second commit

---
+++ assets/header.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Escape Poverty Africa</title>
+	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/owl-carousel/assets/owl.carousel.css">
+	<link rel="stylesheet" href="assets/css/animate.css">
+	<link rel="stylesheet" href="assets/css/meanmenu.css">
+	<link rel="stylesheet" href="assets/css/nivo-lightbox.css">
+	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+	<link rel="stylesheet" href="style.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+
+	<!-- header -->
+	<header>
+		<div id="search-bar">
+			<div class="container">
+				<div class="row">
+					<form action="#" name="search" class="col-xs-12">
+						<input type="text" name="search" placeholder="Type and Hit Enter"><i id="search-close" class="fa fa-close"></i>
+					</form>
+				</div>
+			</div>
+		</div>
+		<nav class="navigation">
+			<div class="container">
+				<div class="row">
+					<div class="logo-wrap col-md-3 col-xs-6">
+						<a href="index.html"><img src=""></a>
+					</div>
+					<div class="menu-wrap col-md-8 ">
+						<ul class="menu">
+							<li class="active">
+								<a href="index.html">Home</a>
+							</li>
+							<li>
+								<span>About</span>
+								<ul class="submenu">
+									<li><a href="about-us.html">WHO IS ESCAPE POVERTY(EP)-AFRICA?</a></li>
+									
+									
+								</ul>
+							</li>
+							<li>
+								<a href="service.html">Services</a>
+							</li>
+							<li>
+								<span><a href="services.html">The Team</a></span>
+								<ul class="submenu">
+									<li><a href="service-single.html">ADVISORY BOARD</a></li>
+									<li><a href="service-single (copy).html" class="active">MANAGEMENT TEAM</a></li>
+								</ul>
+							</li>
+							<li>
+								<a href="gallery.html">Gallery</a>
+							</li>
+							<li>
+								<a href="blog.html">News</a>
+							</li>
+							<li>
+								<a href="contact-us.html">Contact Us</a>
+							</li>
+						</ul>
+					</div>
+					<div class="col-md-1 col-xs-6">
+						<div id="search-toggle">
+							<i class="fa fa-search"></i>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<!--[ MOBILE-MENU-AREA START ]--> 
+			<div class="mobile-menu-area">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 col-sm-12">
+							<div class="mobile-area">
+								<div class="mobile-menu">
+									<nav id="mobile-nav">
+										<ul>
+											<li><a href="index.html">Home </a></li>
+											<li><a href="about-us.html"> About</a></li>
+											<li><a href="contact-us.html">The Team</a></li>
+											<li><a href="contact-us.html">News</a></li>
+										</ul>
+									</nav>
+								</div>	
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!--[ MOBILE-MENU-AREA END  ]-->	
+		</nav>
+	</header>
